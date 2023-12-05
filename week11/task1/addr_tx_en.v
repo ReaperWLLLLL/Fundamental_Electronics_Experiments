20d570bf91c7e17c8f7a8a40a89496ea553ef0bd
reaperwlllll <lei-wang22@mails.tsinghua.edu.cn>
2023-12-05 19:09:10 +0800

    add some comments

--- week11/task1/addr_tx_en.v
+++ week11/task1/addr_tx_en.v
@@ -1,68 +1,72 @@
 // a idiot tried to write a counter in the state machine, and he failed.
 // maybe put the counter in this module will work.
+
 module addr_tx_en #(
     parameter FRAMENUM = 60
 ) (
-    input clk,
-    input clk_origin,
-    input rst,
-    input [15:0] data_temp,
-    output reg [15:0] data_out,
-    output reg [7:0] addr,
-    output reg tx_en
+    input clk,  // Clock input
+    input clk_origin,  // Original clock input
+    input rst,  // Reset input
+    input [15:0] data_temp,  // Temporary data input
+    output reg [15:0] data_out,  // Output data
+    output reg [7:0] addr,  // Address output
+    output reg tx_en  // Transmit enable output
 );
-    reg [5:0] frame_cnt;
+
+    reg [5:0] frame_cnt;  // Frame counter
+
     always @(posedge clk, posedge rst) begin
         if (rst) begin
-            addr <= 8'b00000000;
-            frame_cnt <= 6'b000000;
+            addr <= 8'b00000000;  // Reset address
+            frame_cnt <= 6'b000000;  // Reset frame counter
         end else begin
-            if (addr == 8'b11111111) begin
-                addr <= 8'b00000000;
+            if (addr == 8'b11111111) begin  // Check if address is at maximum value
+                addr <= 8'b00000000;  // Reset address
             end else begin
-                if (frame_cnt == FRAMENUM - 1) begin
-                    addr <= addr;
-                    data_out <= 16'h4545;
-                    frame_cnt <= 6'b000000;
-                end else if (frame_cnt == 0) begin
-                    addr <= addr;
-                    data_out <= 16'h5353;
-                    frame_cnt <= frame_cnt + 1'b1;
+                if (frame_cnt == FRAMENUM - 1) begin  // Check if frame counter is at maximum value
+                    addr <= addr;  // Keep current address
+                    data_out <= 16'h4545;  // Set output data
+                    frame_cnt <= 6'b000000;  // Reset frame counter
+                end else if (frame_cnt == 0) begin  // Check if frame counter is at minimum value
+                    addr <= addr;  // Keep current address
+                    data_out <= 16'h5353;  // Set output data
+                    frame_cnt <= frame_cnt + 1'b1;  // Increment frame counter
                 end else begin
-                    addr <= addr + 1'b1;
-                    data_out <= data_temp;
-                    frame_cnt <= frame_cnt + 1'b1;
+                    addr <= addr + 1'b1;  // Increment address
+                    data_out <= data_temp;  // Set output data
+                    frame_cnt <= frame_cnt + 1'b1;  // Increment frame counter
                 end
             end
         end
     end
 
-    reg pulse1, pulse2, pulse3;
-    wire clk_posedge;
-    always @(posedge clk_origin, posedge rst) begin
-        if (rst) begin
-            pulse1 <= 1'b0;
-            pulse2 <= 1'b0;
-            pulse3 <= 1'b0;
-        end else begin
-            pulse1 <= clk;
-            pulse2 <= pulse1;
-            pulse3 <= pulse2;
-        end
-    end
-    assign clk_posedge = pulse2 & ~pulse3;
+    reg pulse1, pulse2, pulse3;  // Clock pulse signals
+    wire clk_posedge;  // Positive edge clock signal
 
     always @(posedge clk_origin, posedge rst) begin
         if (rst) begin
-            tx_en <= 1'b0;
+            pulse1 <= 1'b0;  // Reset pulse1
+            pulse2 <= 1'b0;  // Reset pulse2
+            pulse3 <= 1'b0;  // Reset pulse3
         end else begin
-            if (clk_posedge) begin
-                tx_en <= 1'b1;
+            pulse1 <= clk;  // Assign pulse1 with clock input
+            pulse2 <= pulse1;  // Assign pulse2 with pulse1
+            pulse3 <= pulse2;  // Assign pulse3 with pulse2
+        end
+    end
+
+    assign clk_posedge = pulse2 & ~pulse3;  // Calculate positive edge clock signal
+
+    always @(posedge clk_origin, posedge rst) begin
+        if (rst) begin
+            tx_en <= 1'b0;  // Reset transmit enable
+        end else begin
+            if (clk_posedge) begin  // Check if positive edge clock signal is high
+                tx_en <= 1'b1;  // Set transmit enable
             end else begin
-                tx_en <= 1'b0;
+                tx_en <= 1'b0;  // Reset transmit enable
             end
         end
     end
-
 
 endmodule
--- week11/task1/uart_frame_transmit_top.v
+++ week11/task1/uart_frame_transmit_top.v
@@ -1,57 +1,52 @@
-`include "addr_tx_en.v"
-`include "sin_16_rom.v"
-`include "fre_div.v"
-`include "uart_byte_controller.v"
-`include "uart_2byte_controller_withoutHeader.v"
-`include "uart_tx_byte.v"
+// This module is the top-level module for transmitting UART frames.
+// It includes various sub-modules for address generation, data generation, and UART transmission.
+
+`include "addr_tx_en.v" // Include module for address and transmit enable generation.
+`include "sin_16_rom.v" // Include module for generating sine wave data.
+`include "fre_div.v" // Include module for clock frequency division.
+`include "uart_byte_controller.v" // Include module for byte-level UART transmission control.
+`include "uart_2byte_controller_withoutHeader.v" // Include module for 2-byte UART transmission control without header.
+`include "uart_tx_byte.v" // Include module for UART byte transmission.
+
 module uart_frame_transmit_top (
-    input  clk,
-    input  rst,
-    output sci_tx
+    input  clk, // Clock input
+    input  rst, // Reset input
+    output sci_tx // UART transmit output
 );
 
-    wire clk_div_addr;
-    fre_div #(25'd12500) uut1 (
+    wire clk_div_addr; // Clock divided address signal
+    fre_div #(25'd12500) uut1 ( // Instantiate frequency divider module
         .clk(clk),
         .rst(rst),
         .clk_div_addr(clk_div_addr)
     );
 
-    wire [7:0] addr;
-    wire send_en;
-    wire [15:0] data_out;  
+    wire [7:0] addr; // Address signal
+    wire send_en; // Transmit enable signal
+    wire [15:0] data_out; // Output data signal
 
-    addr_tx_en uut2 (
+    addr_tx_en uut2 ( // Instantiate address and transmit enable module
         .clk_origin(clk),
         .clk(clk_div_addr),
         .rst(rst),
         .addr(addr),
         .data_temp(data_temp),
         .data_out(data_out),
-        .tx_en(send_en)//this maybe confusing, because i copy the code from last week and do not change the name
+        .tx_en(send_en)
     );
 
-    wire [15:0] data_temp;
-    sin_16_rom uut3 (
+    wire [15:0] data_temp; // Temporary data signal
+    sin_16_rom uut3 ( // Instantiate sine wave ROM module
         .address(addr),
         .clock(clk),
         .q(data_temp)
     );
 
-    wire tx_en;
-    wire [7:0] tx_d;
-    wire tx_done;
-    // uart_byte_controller #(12) uut4 (
-    //     .clk(clk),
-    //     .rst(rst),
-    //     .send_en(send_en),
-    //     .tx_en(tx_en),
-    //     .tx_done(tx_done),
-    //.data(data_temp),
-    //     .tx_d(tx_d)
-    // );
+    wire tx_en; // Transmit enable signal
+    wire [7:0] tx_d; // Transmit data signal
+    wire tx_done; // Transmission completion signal
 
-    uart_2byte_controller_withoutHeader uut4 (
+    uart_2byte_controller_withoutHeader uut4 ( // Instantiate 2-byte UART controller module without header
         .clk(clk),
         .rst(rst),
         .send_en(send_en),
@@ -61,7 +56,7 @@ module uart_frame_transmit_top (
         .tx_d(tx_d)
     );
 
-    uart_tx_byte uut5 (
+    uart_tx_byte uut5 ( // Instantiate UART byte transmission module
         .clk(clk),
         .rst(rst),
         .tx_en(tx_en),
@@ -69,14 +64,5 @@ module uart_frame_transmit_top (
         .rx_d(tx_d),
         .sci_tx(sci_tx)
     );
-
-
-
-
-
-
-
-
-
 
 endmodule
